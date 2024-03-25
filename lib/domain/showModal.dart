176ecfa2c9d530951c7ca1beb0fc3ae16a0909e6
szamotine <77281802+szamotine@users.noreply.github.com>
2024-03-24 18:40:00 -0700

    Created dropdown ui and backend for model list

---
+++ lib/domain/showModal.dart
@@ -0,0 +1,32 @@
+import 'package:chat_gpt/feature/widgets/drop_down.dart';
+import 'package:flutter/material.dart';
+
+import '../data/constants.dart';
+import '../feature/widgets/text_widget.dart';
+
+class ShowModal {
+  static Future<void> showModalSheet({required BuildContext context}) async {
+    await showModalBottomSheet(
+      shape: const RoundedRectangleBorder(borderRadius: BorderRadius.vertical(top: Radius.circular(20))),
+      backgroundColor: kScaffoldBackgroundColor,
+      context: context,
+      builder: (context) {
+        return const Padding(
+          padding: EdgeInsets.all(18.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Flexible(
+                child: TextWidget(
+                  label: 'Chosen Model: ',
+                  fontSize: 16,
+                ),
+              ),
+              Flexible(flex: 2, child: ModelDropDownWidget()),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
---
+++ lib/feature/widgets/drop_down.dart
@@ -0,0 +1,27 @@
+import 'package:chat_gpt/data/constants.dart';
+import 'package:flutter/material.dart';
+
+class ModelDropDownWidget extends StatefulWidget {
+  const ModelDropDownWidget({super.key});
+
+  @override
+  State<ModelDropDownWidget> createState() => _ModelDropDownWidgetState();
+}
+
+class _ModelDropDownWidgetState extends State<ModelDropDownWidget> {
+  String currentModel = "Model1";
+
+  @override
+  Widget build(BuildContext context) {
+    return DropdownButton(
+      dropdownColor: kScaffoldBackgroundColor,
+      items: ModelsItem,
+      value: currentModel,
+      onChanged: (value) {
+        setState(() {
+          currentModel = value.toString();
+        });
+      },
+    );
+  }
+}
